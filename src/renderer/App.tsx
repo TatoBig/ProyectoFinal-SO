c6940b579482ce62f91f12fd983409ed289d98ae
TatoBig <Tatobig@gmail.com>
2022-05-01 22:03:23 -0600

    Agregado el frontend hecho con el starter de electron-react, movidos los archivos en python a /api

---
+++ src/renderer/App.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { useForm } from 'react-hook-form';
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+type FormValues = {
+  ip: string
+}
+
+const Hello = () => {
+  const { register, handleSubmit } = useForm<FormValues>();
+
+  const onSubmit = (data: FormValues) => {
+    fetch('https://rickandmortyapi.com/api/character/68')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
+  return (
+    <div>
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input {...register('ip')} type="text" style={{ padding: 4 }} />
+        <input type="text" style={{ padding: 4 }} />
+        <button type="submit">Button</button>
+      </form>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
