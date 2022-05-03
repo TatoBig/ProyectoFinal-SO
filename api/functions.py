2499d55d4d194b8af0ff8ae796d7b22fb429af0a
Paolo Veliz <paoloveliz55@gmail.com>
2022-05-02 18:29:33 -0600

    Se creo la funcion para poder verificar si una operacion pertenece a una red

--- api/functions.py
+++ api/functions.py
@@ -53,3 +53,75 @@ def ConvertirABinario(numero):
     nuevo_modulo.append(0)
   result = nuevo_modulo + modulos
   return result
+
+def PerteneceARed(ip_base,ip_comparacion):
+  #Declaracion de las variables
+  matriz = [['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''],
+           ['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',],
+           ['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','']]
+  octeto_ip_base_1 = 0
+  octeto_ip_base_2 = 0
+  octeto_ip_base_3 = 0
+  octeto_ip_base_4 = 0
+  octeto_id_comparacion_1 = 0
+  octeto_id_comparacion_2 = 0
+  octeto_id_comparacion_3 = 0
+  octeto_id_comparacion_4 = 0
+  
+  #Seccion paar encontrar los string de la ip base
+  #Octeto 1
+  temp = ip_base
+  primer_string = ip_base[0:ip_base.find('.')]
+  temp = ip_base[ip_base.find('.') + 1:len(ip_base)]
+  octeto_ip_base_1 = int(primer_string)
+  #Octeto 2
+  segundo_string = temp[0:temp.find('.')]
+  temp = temp[temp.find('.') + 1:len(temp)]
+  octeto_ip_base_2 = int(segundo_string)
+  #Octeto 3
+  tercer_string = temp[0:temp.find('.')]
+  temp = temp[temp.find('.') + 1:len(temp)]
+  octeto_ip_base_3 = int(tercer_string)
+  # Octeto 4
+  cuarto_string = temp[0:temp.find('/')]
+  temp = temp[temp.find('/') + 1:len(temp)]
+  octeto_ip_base_4 = int(cuarto_string)
+  #Mascara de red
+  quinto_string = temp
+  mascara_de_red = int(quinto_string)
+ 
+  #Seccion para poder encontrar los octetos del ip base comparacion
+   #Octeto 1
+  temp1 = ip_comparacion
+  primer_string_2 = ip_comparacion[0:ip_comparacion.find('.')]
+  temp1 = ip_comparacion[ip_comparacion.find('.') + 1:len(ip_comparacion)]
+  octeto_id_comparacion_1 = int(primer_string_2)
+  #Octeto 2
+  segundo_string_2 = temp1[0:temp1.find('.')]
+  temp1 = temp1[temp1.find('.') + 1:len(temp1)]
+  octeto_id_comparacion_2 = int(segundo_string_2)
+  #Octeto 3
+  tercer_string_2 = temp1[0:temp1.find('.')]
+  temp1 = temp1[temp1.find('.') + 1:len(temp1)]
+  octeto_id_comparacion_3 = int(tercer_string_2)
+  # Octeto 4
+  cuarto_string_2 = temp1
+  octeto_id_comparacion_4 = int(cuarto_string_2)
+
+  #Convertir los numeros decimales a binarios
+  binario_de_mascara = Encontrar_red(mascara_de_red)
+  binario_de_base = Obtener_ip_en_binario(octeto_ip_base_1,octeto_ip_base_2,octeto_ip_base_3,octeto_ip_base_4)
+  binario_de_comparacion = Obtener_ip_en_binario(octeto_id_comparacion_1,octeto_id_comparacion_2,octeto_id_comparacion_3,octeto_id_comparacion_4)
+
+  matriz[0] = binario_de_comparacion
+  matriz[1] = binario_de_mascara
+  #Ahora toca saber si es la misma, para ello vamos a multiplicar el binario de ip comparacion con el de la mascara de la red, por que es lo mismo que hacer un AND
+  pos = 0
+  while (pos < len(matriz[0])):
+    matriz[2][pos] = binario_de_comparacion[pos] * binario_de_mascara[pos]
+    pos = pos + 1
+
+  if (matriz[2] == binario_de_base):
+    return "Si pertence a red"
+  else:
+    return "No pertence a red"
